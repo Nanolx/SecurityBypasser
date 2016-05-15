d0951a33c4c0bd5e2af8bce04c2c2bfb19089f50
Christopher Roy Bratusek <nano@jpberlin.de>
2016-05-15 11:28:04 +0200

    Version 1.0.8

--- app/src/main/java/org/nanolx/miitomosecuritybypasser/XposedEntryPoint.java
+++ app/src/main/java/org/nanolx/miitomosecuritybypasser/XposedEntryPoint.java
@@ -1,14 +1,21 @@
 package org.nanolx.securitybypasser;
-import de.robv.android.xposed.*;
-import de.robv.android.xposed.callbacks.*;
-import de.robv.android.xposed.XC_MethodHook.*;
-import android.app.*;
+
+import android.content.Context;
+import android.app.Activity;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedEntryPoint implements IXposedHookLoadPackage
 {
 
 	@Override
-	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam param) throws Throwable
+	public void handleLoadPackage(LoadPackageParam param) throws Throwable
 	{
 						// Miitomo
 		if (param.packageName.equalsIgnoreCase("com.nintendo.zaaa"))
@@ -18,7 +25,7 @@ public class XposedEntryPoint implements IXposedHookLoadPackage
 		XposedHelpers.findAndHookMethod("jp.co.nintendo.SecureController", param.classLoader, "ahS", new XC_MethodReplacement(){
 
 				@Override
-				protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
 				{
 					XposedBridge.log("Nanolx Security Bypasser: Nintendo is trying to stop us, but I won't let it happen! (Hooked SecureController.ahS())");
 					return null;
@@ -31,7 +38,7 @@ public class XposedEntryPoint implements IXposedHookLoadPackage
 		XposedHelpers.findAndHookMethod("jp.co.nintendo.SecureController", param.classLoader, "ahT", new XC_MethodReplacement(){
 
 				@Override
-				protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
 				{
 					XposedBridge.log("Nanolx Security Bypasser: It's debug time! (Set BuildConfig.DEBUG to true)");
 					return null;
@@ -41,7 +48,7 @@ public class XposedEntryPoint implements IXposedHookLoadPackage
 
 		XposedHelpers.findAndHookMethod("jp.co.nintendo.SecureController", param.classLoader, "L", Activity.class, new XC_MethodReplacement(){
 				@Override
-				protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
 				{
 					XposedBridge.log("Nanolx Security Bypasser: Nintendo, I'm afraid I can't let you do that... (Hooked SecureController.L())");
 					return null;
@@ -52,7 +59,7 @@ public class XposedEntryPoint implements IXposedHookLoadPackage
 		XposedHelpers.findAndHookMethod("jp.co.nintendo.v", param.classLoader, "dp", boolean.class, new XC_MethodReplacement(){
 
 				@Override
-				protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam p1) throws Throwable
+				protected Object replaceHookedMethod(MethodHookParam p1) throws Throwable
 				{
 					XposedBridge.log("Nanolx Security Bypasser: All you logcat output are belong to us! (Trying to catch v.dp(), logcat output should be available now!)");
 					return null;
@@ -63,20 +70,31 @@ public class XposedEntryPoint implements IXposedHookLoadPackage
 		XposedHelpers.findAndHookMethod("jp.co.nintendo.NativeSystem", param.classLoader, "nativeAbort", new XC_MethodReplacement(){
 
 				@Override
-				protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam p1) throws Throwable
+				protected Object replaceHookedMethod(MethodHookParam p1) throws Throwable
 				{
 					XposedBridge.log("Nanolx Security Bypasser: Don't leave mii! (Trying to catch NativeSystem.nativeAbort())");
 					return null;
 				}
 
 		});
-		
+
 		XposedHelpers.findAndHookMethod("jp.co.nintendo.SecureController", param.classLoader, "checkEmulator", new XC_MethodReplacement(){
 
 				@Override
-				protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam p1) throws Throwable
+				protected Object replaceHookedMethod(MethodHookParam p1) throws Throwable
 				{
 					XposedBridge.log("Nanolx Security Bypasser: Mii a emulator? Nope! (Hooked SecureController.checkEmulator())");
+					return false;
+				}
+
+			});
+
+		XposedHelpers.findAndHookMethod("org.cocos2dx.lib.Cocos2dxActivity", param.classLoader, "isAndroidEmulator", new XC_MethodReplacement(){
+
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam p1) throws Throwable
+				{
+					XposedBridge.log("Nanolx Security Bypasser: Mii a emulator? Nope! (Hooked Cocos2dxActivity.isAndroidEmulator())");
 					return false;
 				}
 
@@ -89,18 +107,70 @@ public class XposedEntryPoint implements IXposedHookLoadPackage
 
 		XposedBridge.log("Nanolx Security Bypasser: Entering Yokai Watch Wibble Wobble (US)... Ready... Set... GO!");
 
-		XposedBridge.log("Nanolx Security Bypasser: Level5 is trying to stop us, but I won't let it happen!");
-		XposedBridge.log("(Hooked com.example.sgf.MainActivity.checkRoot())");
 		XposedHelpers.findAndHookMethod("com.example.sgf.MainActivity",
 			param.classLoader,
 			"checkRoot",
-			XC_MethodReplacement.returnConstant(false));
+			new XC_MethodReplacement()
+			{
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
+				{
+					XposedBridge.log("Nanolx Security Bypasser: Level5 is trying to stop us, but I won't let it happen!");
+					XposedBridge.log("(Hooked com.example.sgf.MainActivity.checkRoot())");
+					return false;
+				}
+			});
 
-		XposedBridge.log("Nanolx Security Bypasser: Hey, don't look for /system!!!");
 		XposedHelpers.findAndHookMethod("com.mparticle.b.d",
 			param.classLoader,
 			"h",
-			XC_MethodReplacement.returnConstant(false));
+			Context.class,
+			new XC_MethodReplacement()
+			{
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
+				{
+					XposedBridge.log("Nanolx Security Bypasser: Hey, don't sniff into /system!");
+					XposedBridge.log("(com.mparticle.b.d.h())");
+					return false;
+				}
+			});
+
+		XposedHelpers.findAndHookMethod("com.hangame.hsp.util.DeviceInfoUtil",
+			param.classLoader,
+			"isRunningOnEmulator",
+			new XC_MethodReplacement()
+			{
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
+				{
+					XposedBridge.log("Nanolx Security Bypasser: Hey, don't sniff into /system!");
+					XposedBridge.log("(com.hangame.hsp.util.DeviceInfoUtil.isRunningOnEmulator())");
+					return false;
+				}
+			});
+
+		}
+							// Pokemon Jukebox
+		else if (param.packageName.equalsIgnoreCase("jp.pokemon.music"))
+		{
+
+		XposedBridge.log("Nanolx Security Bypasser: Entering Pokemon Jukebox... Ready... Set... GO!");
+
+		XposedHelpers.findAndHookMethod("com.pokemon.music.d.k",
+			param.classLoader,
+			"c",
+			Context.class,
+			new XC_MethodReplacement()
+			{
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
+				{
+					XposedBridge.log("Nanolx Security Bypasser: Pokemon Company is trying to stop us, but I won't let it happen!");
+					XposedBridge.log("(Hooked com.pokemon.music.d.k.c())");
+					return false;
+				}
+			});
 
 		}
 		else
